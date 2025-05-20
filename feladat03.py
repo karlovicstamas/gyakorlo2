9536aa114f718d71bfd8c2a0080d32215c4bbae8
Petrik Diak <diak@petrik.hu>
2025-05-20 13:29:44 +0200

    feladat03 kesz(majdnem)

--- feladat03.py
+++ feladat03.py
@@ -19,12 +19,56 @@ def beolvasas():
             kutyakiallitas.append(kutya)
     f.close()
 
-#1. feladat
+#2. feladat
 def db():
     print(f"{len(kutyakiallitas)} kutya szerepel a listában")
+
+#3. feladat
+def fajta():
+    fajta = {}
+    for item in kutyakiallitas:
+        if item.fajta not in fajta:
+            fajta[item.fajta] = 1
+    
+    print(f"{len(fajta)} különböző fajta van a listában")
+
+#4. feladat
+def legidosebb():
+    legidosebb = 0
+    legidosebb_kutya = ""
+    for item in kutyakiallitas:
+        if item.eletkor > legidosebb:
+            legidosebb = item.eletkor
+            legidosebb_kutya = item.nev
+
+    print(f"{legidosebb_kutya} a legidősebb kutya és {legidosebb} éves")
+
+#5. feladat
+def gazdi():
+    global gazda
+    gazda = str(input("Kérem adja meg egy gazdinak a nevét, akinek a kutyáit ki szeretné íratni: "))
+    for item in kutyakiallitas:
+        if item.gazdi == gazda:
+            print(item)
+
+#6. feladat
+def ujfajl():
+    with open(f"{gazda}.txt", "w", encoding = "utf-8") as ujfajl:
+        for item in kutyakiallitas:
+            if item.gazdi == gazda:
+                kutya_neve = item.nev
+                kutya_fajtaja = item.fajta
+                kutya_eletkora = item.eletkor
+                kutya_gazdija = item.gazdi
+                ujfajl.write(f"{kutya_neve} ({kutya_fajtaja}, {kutya_eletkora}) - {kutya_gazdija} \n")
+    ujfajl.close()
 
 def main():
     beolvasas()
     db()
+    fajta()
+    legidosebb()
+    gazdi()
+    ujfajl()
 
 main()
